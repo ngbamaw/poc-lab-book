2c5ce05e2436560e6cda24801aa929aeaf8894b7
NGBAMA William <william.ngbama@gmail.com>
2022-09-27 17:54:56 +0200

    full width

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import useScanner from './hooks/useScanner'
+import useWindowDimensions from './hooks/useWindowDimensions';
 
 function App() {
+  const { width, height } = useWindowDimensions();
   const { code, start } = useScanner("reader", {
     fps: 10,
-    qrbox: { width: 200, height: 200 },
+    qrbox: { width: width - 24, height: width - 24 },
   });
+  
+  const size = { width, /* height */ };
 
   React.useEffect(() => {
     console.log(code);
   }, [code])
   return (
     <div className="App">
-      <div id="reader" style={{ width: 300 }}></div>
+      <div id="reader" style={size}></div>
       <button onClick={() => start()}>Scan</button>
       {code && <p>Book: {code.decodedText}</p>}
     </div>
